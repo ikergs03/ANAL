c471b491613f2319ea59619ec8e04cb0c1dd520e
Iker González Sánchez <101722105+ikergs03@users.noreply.github.com>
2023-11-02 15:34:12 +0000

    ff

--- P2/sorting.c
+++ P2/sorting.c
@@ -11,6 +11,8 @@
 
 
 #include "sorting.h"
+#include <stddef.h>
+#include <stdlib.h>
 
 /***************************************************/
 /* Function: SelectSort    Date:                   */
@@ -69,6 +71,71 @@ int min(int* array, int ip, int iu)
   }
 
   return min;
+}
+
+//Práctica 2
+
+int mergesort(int* tabla, int ip, int iu) 
+{
+  if(!tabla || ip < 0 || iu < 0 || ip > iu) return ERR;
+
+  if(ip == iu) return 0;
+
+  int imedio = (ip + iu) / 2;
+  int ob1 = mergesort(tabla, ip, imedio);
+  int ob2 = mergesort(tabla, imedio + 1, iu);
+  int ob3 = merge(tabla, ip, iu, imedio);
+  
+
+  return ob1 + ob2 + ob3;
+}
+
+
+int merge(int* tabla, int ip, int iu, int imedio)
+{
+  if(!tabla || ip < 0 || iu < 0 || ip > iu) return ERR;
+
+  int* aux = NULL;
+  int i = ip, j = imedio + 1, k = 0, ob = 0;
+
+  aux = (int*)malloc(sizeof(int) * (iu - ip + 1));
+  if(!aux) return ERR;
+
+  while(i <= imedio && j <= iu) {
+    if(tabla[i] < tabla[j]) {
+      aux[k] = tabla[i];
+      i++;
+    }
+    else {
+      aux[k] = tabla[j];
+      j++;
+    }
+    ob++;
+    k++;
+  }
+
+  if(i > imedio) {
+    while(j <= iu) {
+      aux[k] = tabla[j];
+      j++;
+      k++;
+    }
+  }
+  else {
+    while(i <= imedio) {
+      aux[k] = tabla[i];
+      i++;
+      k++;
+    }
+  }
+
+  for(i = ip, k = 0; i <= iu; i++, k++) {
+    tabla[i] = aux[k];
+  }
+
+  free(aux);
+
+  return ob;
 }
 
 
