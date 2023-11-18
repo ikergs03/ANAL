0815ea1b825a4da412f341f11ab0f57781ae7e05
cagadon de gonzalo <iker.gonzalezs@estudiante.uam.es>
2023-11-18 18:10:14 +0100

    avances en p2

--- P2/sorting.c
+++ P2/sorting.c
@@ -83,8 +83,11 @@ int mergesort(int* tabla, int ip, int iu)
 
   int imedio = (ip + iu) / 2;
   int ob1 = mergesort(tabla, ip, imedio);
+  if (ob1 == ERR) return ERR;
   int ob2 = mergesort(tabla, imedio + 1, iu);
+  if (ob2 == ERR) return ERR;
   int ob3 = merge(tabla, ip, iu, imedio);
+  if (ob3 == ERR) return ERR;
   
 
   return ob1 + ob2 + ob3;
@@ -136,6 +139,65 @@ int merge(int* tabla, int ip, int iu, int imedio)
   free(aux);
 
   return ob;
+}
+
+
+int partition(int* tabla, int ip, int iu, int* pos) {
+    if (!tabla || ip < 0 || iu < 0 || ip > iu) return ERR;
+
+    int pivot = tabla[ip];
+    int i = ip + 1;
+
+    for (int j = ip + 1; j <= iu; j++) {
+        if (tabla[j] < pivot) {
+            // Intercambiar elementos
+            int temp = tabla[j];
+            tabla[j] = tabla[i];
+            tabla[i] = temp;
+            i++;
+        }
+    }
+
+    // Colocar el pivote en su posición final
+    tabla[ip] = tabla[i - 1];
+    tabla[i - 1] = pivot;
+
+    *pos = i - 1;  // Posición final del pivote
+    return iu - ip;  // Número de operaciones básicas
+}
+
+int median(int* tabla, int ip, int iu, int* pos) {
+    if (!tabla || ip < 0 || iu < 0 || ip > iu) return ERR;
+
+    *pos = ip;
+    return 0;
+}
+
+int quicksort(int* tabla, int ip, int iu) {
+    if (!tabla || ip < 0 || iu < 0 || ip > iu) return ERR;
+
+    if (ip < iu) {
+        int pos;
+        int ob = median(tabla, ip, iu, &pos);
+        if (ob == ERR) return ERR;
+
+        // Intercambiar el pivote con el primer elemento
+        int temp = tabla[ip];
+        tabla[ip] = tabla[pos];
+        tabla[pos] = temp;
+
+        int ob_partition = partition(tabla, ip, iu, &pos);
+        if (ob_partition == ERR) return ERR;
+
+        int ob1 = quicksort(tabla, ip, pos - 1);
+        int ob2 = quicksort(tabla, pos + 1, iu);
+
+        if (ob1 == ERR || ob2 == ERR) return ERR;
+
+        return ob + ob_partition + ob1 + ob2;
+    }
+
+    return 0;  // Caso base: ya está ordenado
 }
 
 
